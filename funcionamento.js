fd3f2cb4eaee973838c773eb40294099e0809dbd
nords-codes <nordeley_corradini@estudante.sesisenai.org.br>
2026-03-18 09:32:20 -0300

    feat - adicionei os comandos no java script

--- funcionamento.js
+++ funcionamento.js
@@ -1,8 +1,45 @@
-function calculo(){
 
-    let numero_1
-    let numero_2
-    let soma = 0
-    let subtracao = 0
 
+
+
+
+function soma() {
+    let resultado = 0;
+    let n1 =  Number(document.getElementById("n1").value);
+    let n2 = Number (document.getElementById("n2").value);
+
+    resultado = n1 + n2;
+    console.log(resultado);
+    document.getElementById("Resultado").innerHTML = resultado;
+}
+
+function sub() {
+ let resultado = 0;
+    let n1 =  Number(document.getElementById("n1").value);
+    let n2 = Number (document.getElementById("n2").value);
+
+    resultado = n1 - n2;
+    document.getElementById("Resultado").innerHTML = resultado;
+console.log(resultado);
+}
+function div() {
+ let resultado = 0;
+    let n1 =  Number(document.getElementById("n1").value);
+    let n2 = Number (document.getElementById("n2").value);
+
+    if (n2 === 0) {
+        alert("resultado inexistente")
+    }
+    resultado = n1 / n2;
+    document.getElementById("Resultado").innerHTML = resultado;
+console.log(resultado);
+}
+function mult() {
+ let resultado = 0;
+    let n1 =  Number(document.getElementById("n1").value);
+    let n2 = Number (document.getElementById("n2").value);
+
+    resultado = n1 * n2
+    document.getElementById("Resultado").innerHTML = resultado;
+console.log(resultado);
 }
